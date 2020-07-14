80111f5c0419f37aab94ab306dd7e7f7cfb503ec
Muh Arsyi Azimin <arsyiazimin@gmail.com>
2020-07-14 23:18:29 +0700

    add service in open api

--- src/modules/open-api/open-api.module.ts
+++ src/modules/open-api/open-api.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OpenApiController } from './controller/open-api/open-api.controller';
 import { OpenApiService } from './service/open-api/open-api.service';
+import { BlogService } from 'modules/blog/service/blog/blog.service';
+import { BlogModule } from 'modules/blog/blog.module';
 
 @Module({
   controllers: [OpenApiController],
-  providers: [OpenApiService]
+  providers: [OpenApiService],
+  imports: [BlogModule]
 })
 export class OpenApiModule {}
